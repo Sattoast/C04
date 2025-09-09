ef7e568c2375bab5f9a7e0ffa2dbf709ea5ca056
Ethan Fernandez <etfernan@c1r4p10.42lehavre.fr>
2025-09-09 16:10:48 +0200

    modifie

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -10,11 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-int	ft_atoi(char *str)
+int	check_sign(const char *str, int i, int *negative)
 {
-	while ((str != 'A' && str != 'Z'))
-		|| ((str != 'a' && str != 'z'));
-	return (0);
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			*negative = -1;
+		i++;
+	}
+	return (i);
 }
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	negative;
+	long	res;
+
+	i = 0;
+	is_neg = 1;
+	res = 0;
+	while ((str[i] == 32 || str[i] >= 9 && str[i] <= 13))
+		i++;
+	i = check_sign(str, i, &negative);
+	while (str[i])
+	{
+		if (str[i] < 48 || str[i] > 57)
+			break ;
+		else
+			res = (res * 10) + str[i] - 48;
+		i++;
+	}
+	return (res * negative);
+}
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	printf("%d\n", ft_atoi("   ---+--+1234ab567"));
+// }
+
